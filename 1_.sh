bc8676e5ab020b90e6eaf9c28e4ef4c03cffc737
Jonathan S. Kwiat <jonathanskwiat@gmail.com>
2020-05-07 12:47:50 -0400

    Got copy function to work

--- 1_.sh
+++ 1_.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
-echo $1
-outbox=$1
-encrypted=$2
-publickey=$3
+startingDirectory=`pwd`
+echo $startingDirectory
 filenameFlag="*.comp"
+function copyEncrypt () {
+	#cd "${startingDirectory}"
+	pwd
+	cp $filename ../encrypted
+}
+
 #echo $outbox $encrypted $publickey
 cd outbox
-function copy_encrypt() {
-	echo $1
-}
 for file in *
 do
 if [[ $file == $filenameFlag ]]; then 
 	 echo "Matches!" $file 
 	 filename=${file%.*}
 	 echo $filename
-	 copy_encrypt () $file $filename
+	 copyEncrypt
  else 
 	 echo "Doesn't match!"
 fi
-}
- #cat "$file"
 done
-greeting () {
-  echo "Hello $1"
-}
-
-greeting "Joe"
