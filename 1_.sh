2562aa6ac23b2bf3f2994a569b4fa8233c5d516f
Jonathan S. Kwiat <jonathanskwiat@gmail.com>
2020-05-07 13:47:09 -0400

    generated public key and line to encrypt

--- 1_.sh
+++ 1_.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 startingDirectory=`pwd`
+publickey=my_secret
 echo $startingDirectory
 filenameFlag="*.comp"
 function copyEncrypt () {
 	#cd "${startingDirectory}"
 	pwd
 	cp $filename ../encrypted
+	gpg --encrypt --recipient 'jonathanskwiat@gmail.com' --output confidential.txt.enc "${filename}"
+	cp $file ../encrypted
 }
 
 #echo $outbox $encrypted $publickey
